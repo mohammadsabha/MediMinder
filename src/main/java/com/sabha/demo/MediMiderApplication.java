06790eb78d20dd1343a94391750e8855bdc367f5
MohammadSabha <mohdsabha@yahoo.com>
2023-05-17 11:54:33 +0300

    initial commit

---
+++ src/main/java/com/sabha/demo/MediMiderApplication.java
@@ -0,0 +1,13 @@
+package com.sabha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediMiderApplication.class, args);
+	}
+
+}
